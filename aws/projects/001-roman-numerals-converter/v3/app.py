313630d6e45b6e5f5a8f16cb6ea3a5c8e92fa17f
E2193 <mstfgkcaydin@gmail.com>
2020-09-30 20:28:48 +0200

    last changes

--- aws/projects/001-roman-numerals-converter/v3/app.py
+++ aws/projects/001-roman-numerals-converter/v3/app.py
@@ -47,4 +47,5 @@ def main_post():
         return render_template('index.html', developer_name='E2193 Mustafa', not_valid=True)
     return render_template('result.html', number_decimal = number , number_roman= convert(number), developer_name='E2193 Mustafa')
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
