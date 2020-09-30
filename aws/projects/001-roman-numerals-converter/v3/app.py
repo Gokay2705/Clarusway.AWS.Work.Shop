359f6659cf4acc812862613af61d6b369e81a1d9
E2193 <mstfgkcaydin@gmail.com>
2020-09-30 19:56:41 +0200

    aws/projects/001-roman-numerals-converter/v3/app.py modified.

--- aws/projects/001-roman-numerals-converter/v3/app.py
+++ aws/projects/001-roman-numerals-converter/v3/app.py
@@ -1,13 +1,3 @@
-#def convert(decimal_num):
-#    roman = {1000: 'M', 900: 'CM', 500: 'D', 400: 'CD', 100: 'C', 90:'XC',
-#             50: 'L', 40: 'XL', 10: 'X', 9: 'IX', 5: 'V', 4: 'IV', 1: 'I'}
-#    num_to_roman = ''
-#    for i in roman.keys():
-#        num_to_roman += roman[i]*(decimal_num//i)
-#        decimal_num %= i
-#    return num_to_roman
-#
-#print(convert(3900))
 
 #def convert_to_roman(num):
 #    roman = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
@@ -21,4 +11,22 @@
 #    
 #print(convert_to_roman(3100))
 
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+def convert(decimal_num):
+    roman = {1000: 'M', 900: 'CM', 500: 'D', 400: 'CD', 100: 'C', 90:'XC',
+             50: 'L', 40: 'XL', 10: 'X', 9: 'IX', 5: 'V', 4: 'IV', 1: 'I'}
+    num_to_roman = ''
+    for i in roman.keys():
+        num_to_roman += roman[i]*(decimal_num//i)
+        decimal_num %= i
+    return num_to_roman
+
+print(convert(3900))
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
 
--- aws/projects/002-milliseconds-converter/app-proje-002.py
+++ aws/projects/002-milliseconds-converter/app-proje-002.py
@@ -1,27 +1,27 @@
-from flask import Flask, url_for, render_template, request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-result = ''
 def convert(milliseconds):
-    # one hour in milliseconds
-    hour_in_milliseconds = 60*60*1000
-    # calculate the hours within given milliseconds
-    hours = milliseconds // hour_in_milliseconds
-    # calculate milliseconds left over when hours subtracted
-    milliseconds_left = milliseconds % hour_in_milliseconds
-    # one minute in milliseconds
-    minutes_in_milliseconds = 60*1000
-    # calculate the minutes within remainder milliseconds
-    minutes = milliseconds_left // minutes_in_milliseconds
-    # calculate milliseconds left over when minutes subtracted
-    milliseconds_left %= minutes_in_milliseconds
-    # calculate the seconds within remainder milliseconds
-    seconds = milliseconds_left // 1000
-    # format the output string
-    return f'{hours} hour/s'*(hours != 0) + f' {minutes} minute/s'*(minutes != 0) + f' {seconds} second/s' *(seconds != 0) or f'just {milliseconds} millisecond/s' * (milliseconds < 1000)
-   
-
+    result = ''
+    while True:
+        # one hour in milliseconds
+        hour_in_milliseconds = 60*60*1000
+        # calculate the hours within given milliseconds
+        hours = milliseconds // hour_in_milliseconds
+        # calculate milliseconds left over when hours subtracted
+        milliseconds_left = milliseconds % hour_in_milliseconds
+        # one minute in milliseconds
+        minutes_in_milliseconds = 60*1000
+        # calculate the minutes within remainder milliseconds
+        minutes = milliseconds_left // minutes_in_milliseconds
+        # calculate milliseconds left over when minutes subtracted
+        milliseconds_left %= minutes_in_milliseconds
+        # calculate the seconds within remainder milliseconds
+        seconds = milliseconds_left // 1000
+        # format the output string
+        return f'{hours} hour/s'*(hours != 0) + f' {minutes} minute/s'*(minutes != 0) + f' {seconds} second/s' *(seconds != 0) or f'just {milliseconds} millisecond/s' * (milliseconds < 1000)
+        
 @app.route("/", methods = ["POST", "GET"])
 def index():
     developer_name = "E2193 Mustafa"
