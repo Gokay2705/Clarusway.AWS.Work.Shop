8a9e5b649d30eeb1d578679613a8740f4e258696
E2193 <mstfgkcaydin@gmail.com>
2020-11-28 15:53:24 +0100

    projets files added,

--- 04-Devops/projects/202-jenkins-pipeline-for-phonebook-app-on-docker-swarm/init.sql
+++ 04-Devops/projects/202-jenkins-pipeline-for-phonebook-app-on-docker-swarm/init.sql
@@ -7,6 +7,6 @@ CREATE TABLE phonebook_db.phonebook(
 
 INSERT INTO phonebook_db.phonebook (name, number)
     VALUES
-        ("Mustafa", "1234567890"),
+        ("Callahan", "1234567890"),
         ("Sergio Taco", "67854"),
         ("Vincenzo Altobelli", "876543554");
